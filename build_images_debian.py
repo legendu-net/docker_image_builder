bd5c513ce2bdf4895298a9e7679975014eca0094
Ben Chuanlong Du <dclong@users.noreply.github.com>
2020-11-28 15:50:07 -0800

    Update build_images_debian.py

--- build_images_debian.py
+++ build_images_debian.py
@@ -3,6 +3,7 @@ import dsutil
 
 images = [
     "https://github.com/dclong/docker-jupyterhub-ds.git",
+    "https://github.com/dclong/docker-jupyterhub-ds.git",
     "https://github.com/dclong/docker-rustpython.git",
 ]
 builder = dsutil.docker.DockerImageBuilder(images, branch="debian")
