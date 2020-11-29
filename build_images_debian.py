e9de96feb93cf12e82e3de771da0fe264ce132e9
Ben Chuanlong Du <dclong@users.noreply.github.com>
2020-11-28 23:00:12 -0800

    Update build_images_debian.py

--- build_images_debian.py
+++ build_images_debian.py
@@ -6,5 +6,5 @@ images = [
     "https://github.com/dclong/docker-vscode-server.git",
     "https://github.com/dclong/docker-rustpython.git",
 ]
-builder = dsutil.docker.DockerImageBuilder(images, branch="debian")
+builder = dsutil.docker.DockerImageBuilder(images, branch="debian_unstable")
 builder.build()
