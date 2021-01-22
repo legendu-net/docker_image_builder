f79cc3496e9ae78e29887301a61d9d02266df1c5
Benjamin Du <longendu@yahoo.com>
2021-01-21 22:32:28 -0800

    refactor code and workflow

---
+++ build_images.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import dsutil.docker
+
+repos = [
+    "https://github.com/dclong/docker-python-portable.git",
+    "https://github.com/dclong/docker-vscode-server.git",
+    "https://github.com/dclong/docker-gitpod.git",
+    "https://github.com/dclong/docker-jupyterhub-julia.git",
+    "https://github.com/dclong/docker-jupyterhub-pytorch.git",
+]
+branch_urls = {
+    "main": repos,
+    "dev": repos,
+    "debian": [
+        "https://github.com/dclong/docker-jupyterhub-ds.git",
+        "https://github.com/dclong/docker-vscode-server.git",
+        "https://github.com/dclong/docker-rustpython.git",
+    ],
+}
+builder = dsutil.docker.DockerImageBuilder(branch_urls)
+builder.build_images()
