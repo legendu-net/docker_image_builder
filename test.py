d3d49b7466d53c8684a1ac85092de088451223dd
Benjamin Du <longendu@yahoo.com>
2021-04-15 18:20:57 -0700

    update test script

--- test.py
+++ test.py
@@ -3,22 +3,20 @@ import datetime
 import dsutil.docker
 
 repos = [
-    "https://github.com/dclong/docker-python-portable.git",
-    "https://github.com/dclong/docker-vscode-server.git",
+    #"https://github.com/dclong/docker-python-portable.git",
+    #"https://github.com/dclong/docker-vscode-server.git",
     "https://github.com/dclong/docker-gitpod.git",
-    "https://github.com/dclong/docker-jupyterhub-julia.git",
-    "https://github.com/dclong/docker-jupyterhub-pytorch.git",
+    #"https://github.com/dclong/docker-jupyterhub-julia.git",
+    #"https://github.com/dclong/docker-jupyterhub-pytorch.git",
 ]
 branch_urls = {
     "main": repos,
-    "dev": repos,
+    #"dev": repos,
     "debian": [
-        "https://github.com/dclong/docker-jupyterhub-ds.git",
-        "https://github.com/dclong/docker-vscode-server.git",
-        "https://github.com/dclong/docker-rustpython.git",
     ],
 }
 builder = dsutil.docker.DockerImageBuilder(branch_urls)
-#builder.build_images(remove=True)
-builder._build_graph()
-builder.save_graph(f"{datetime.date.today()}.yaml")
+#builder._build_graph()
+#builder.save_graph(f"{datetime.date.today()}.yaml")
+builder.build_images(remove=True, push=False)
+
