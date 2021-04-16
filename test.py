e99c03065b1f6e47bcf1e226353e0b13d6261c9c
Benjamin Du <longendu@yahoo.com>
2021-04-16 11:02:04 -0700

    update test.py

--- test.py
+++ test.py
@@ -3,15 +3,15 @@ import datetime
 import dsutil.docker
 
 repos = [
-    #"https://github.com/dclong/docker-python-portable.git",
-    #"https://github.com/dclong/docker-vscode-server.git",
+    "https://github.com/dclong/docker-python-portable.git",
+    "https://github.com/dclong/docker-vscode-server.git",
     "https://github.com/dclong/docker-gitpod.git",
-    #"https://github.com/dclong/docker-jupyterhub-julia.git",
-    #"https://github.com/dclong/docker-jupyterhub-pytorch.git",
+    "https://github.com/dclong/docker-jupyterhub-julia.git",
+    "https://github.com/dclong/docker-jupyterhub-pytorch.git",
 ]
 branch_urls = {
     "main": repos,
-    #"dev": repos,
+    "dev": repos,
     "debian": [
     ],
 }
