a93a495850fb33e1d15c6803844163f0bd9a745e
Ben Du <legendu.net@outlook.com>
2022-08-02 18:13:27 -0700

    adjust order of docker image repos

--- build_images.py
+++ build_images.py
@@ -3,17 +3,16 @@ from dockeree import DockerImageBuilder
 
 
 repos = [
+    "https://github.com/legendu-net/docker-gophernotes.git",
+    "https://github.com/legendu-net/docker-rust-utils.git",
     "https://github.com/legendu-net/docker-python-portable.git",
     "https://github.com/legendu-net/docker-vscode-server.git",
     "https://github.com/legendu-net/docker-gitpod.git",
     "https://github.com/legendu-net/docker-jupyterhub-pytorch.git",
-    "https://github.com/legendu-net/docker-jupyterhub-sagemath.git",
-    "https://github.com/legendu-net/docker-jupyterhub-kotlin.git",
-    "https://github.com/legendu-net/docker-conda-build.git",
     "https://github.com/legendu-net/docker-tensorboard.git",
-    "https://github.com/legendu-net/docker-rust-utils.git",
-    "https://github.com/legendu-net/docker-evcxr_jupyter.git",
-    "https://github.com/legendu-net/docker-gophernotes.git",
+    "https://github.com/legendu-net/docker-conda-build.git",
+    "https://github.com/legendu-net/docker-jupyterhub-kotlin.git",
+    "https://github.com/legendu-net/docker-jupyterhub-sagemath.git",
     #"https://github.com/legendu-net/docker-rustpython.git",
     #"https://github.com/legendu-net/docker-pypy.git",
     #"https://github.com/legendu-net/docker-jupyterhub-julia.git",
