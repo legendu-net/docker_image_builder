aefded32c289b4962490145db03a1899bd10d557
Ben Chuanlong Du <dclong@users.noreply.github.com>
2023-06-27 15:44:59 -0700

    Update build_images.py

--- build_images.py
+++ build_images.py
@@ -25,6 +25,9 @@ branch_urls = {
     "blog": [
         "https://github.com/legendu-net/docker-gitpod.git",
     ],
+    "rust_nightly": [
+        "https://github.com/legendu-net/docker-rust-utils.git",
+    ]
 }
 builder = DockerImageBuilder(branch_urls)
 builder.build_images(remove=True)
