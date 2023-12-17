de1c1471ba2a8825f97390cf374b777f5870f204
Ben Du <longendu@yahoo.com>
2023-12-17 02:03:57 -0800

    update branch_urls due to breaking changes in dockeree 0.6.0

--- build_images.py
+++ build_images.py
@@ -2,32 +2,35 @@
 from dockeree import DockerImageBuilder
 
 
-repos = [
-    "https://github.com/legendu-net/docker-gophernotes.git",
-    "https://github.com/legendu-net/docker-rust-utils.git",
-    "https://github.com/legendu-net/docker-rust-cicd.git",
-    "https://github.com/legendu-net/docker-python-portable.git",
-    "https://github.com/legendu-net/docker-vscode-server.git",
-    "https://github.com/legendu-net/docker-gitpod.git",
-    "https://github.com/legendu-net/docker-jupyterhub-pytorch.git",
-    "https://github.com/legendu-net/docker-tensorboard.git",
-    #"https://github.com/legendu-net/docker-conda-build.git",
-    "https://github.com/legendu-net/docker-jupyterhub-kotlin.git",
-    "https://github.com/legendu-net/docker-jupyterhub-ganymede.git",
-    "https://github.com/legendu-net/docker-jupyterhub-sagemath.git",
-    "https://github.com/legendu-net/docker-rustpython.git",
-    #"https://github.com/legendu-net/docker-pypy.git",
-    #"https://github.com/legendu-net/docker-jupyterhub-julia.git",
-]
+repos = {
+    "https://github.com/legendu-net/docker-gophernotes.git": "",
+    "https://github.com/legendu-net/docker-rust-utils.git": "",
+    "https://github.com/legendu-net/docker-rust-cicd.git": "",
+    "https://github.com/legendu-net/docker-python-portable.git": "",
+    "https://github.com/legendu-net/docker-vscode-server.git": "",
+    "https://github.com/legendu-net/docker-gitpod.git": "",
+    "https://github.com/legendu-net/docker-jupyterhub-pytorch.git": "",
+    "https://github.com/legendu-net/docker-tensorboard.git": "",
+    # "https://github.com/legendu-net/docker-conda-build.git": "",
+    "https://github.com/legendu-net/docker-jupyterhub-kotlin.git": "",
+    "https://github.com/legendu-net/docker-jupyterhub-ganymede.git": "",
+    "https://github.com/legendu-net/docker-jupyterhub-sagemath.git": "",
+    "https://github.com/legendu-net/docker-rustpython.git": "",
+    # "https://github.com/legendu-net/docker-pypy.git": "",
+    # "https://github.com/legendu-net/docker-jupyterhub-julia.git": "",
+}
 branch_urls = {
     "main": repos,
     "dev": repos,
-    "blog": [
-        #"https://github.com/legendu-net/docker-gitpod.git",
-    ],
-    "rust_nightly": [
-        #"https://github.com/legendu-net/docker-rust-utils.git",
-    ],
+    "blog": {
+        # "https://github.com/legendu-net/docker-gitpod.git": "",
+    },
+    "rust_nightly": {
+        # "https://github.com/legendu-net/docker-rust-utils.git": "",
+    },
+    "4.0": {
+        "https://github.com/dclong/docker-jupyterhub.git": "",
+    },
 }
 builder = DockerImageBuilder(branch_urls)
 builder.build_images(remove=True)
